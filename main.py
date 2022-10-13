f00b982807037cac38ebdf32ff106e2dbc9cfbe1
ukalto <maxi.gaber@gmail.com>
2022-10-13 21:33:47 +0200

    initial

---
+++ main.py
@@ -0,0 +1,28 @@
+import speedtest
+import datetime
+
+
+def test():
+    s = speedtest.Speedtest(secure=True)
+    s.get_servers()
+    s.get_best_server()
+    s.download()
+    s.upload()
+    res = s.results.dict()
+    return res["download"], res["upload"], res["ping"]
+
+
+def write():
+    d, u, p = test()
+    data = '-----{}-----\n'.format(datetime.datetime.now())
+    data += 'Download: {:.2f} Kb/s\n'.format(d / 1024)
+    data += 'Upload: {:.2f} Kb/s\n'.format(u / 1024)
+    data += 'Ping: {}\n'.format(p)
+    data += '------------------------------------\n'
+    file1 = open("Data.txt", "a")
+    file1.write(data)
+    file1.close()
+
+
+if __name__ == '__main__':
+    write()
